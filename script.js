8b5412f1a5781e7266d59f711a2fbafb753a1a3f
Muhammad Salmun <muhammedsalmun@gmail.com>
2022-07-28 23:51:18 +0530

    modified UI and moved logo to left in mob view

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const eventsContainers = [...document.querySelectorAll('.events-upcoming')];
 const eventsNxtBtn = [...document.querySelectorAll('.events-nxt-btn')];
 const eventsPreBtn = [...document.querySelectorAll('.events-pre-btn')];
+const ourTeamContainers = [...document.querySelectorAll('.our-team')];
+const ourTeamNxtBtn = [...document.querySelectorAll('.our-team-nxt-btn')];
+const ourTeamPreBtn = [...document.querySelectorAll('.our-team-pre-btn')];
 
 eventsContainers.forEach((item, i) => {
         let containerDimensions = item.getBoundingClientRect();
@@ -14,10 +17,6 @@ eventsContainers.forEach((item, i) => {
             item.scrollLeft -= containerWidth;
         })
     })
-
-const ourTeamContainers = [...document.querySelectorAll('.our-team')];
-const ourTeamNxtBtn = [...document.querySelectorAll('.our-team-nxt-btn')];
-const ourTeamPreBtn = [...document.querySelectorAll('.our-team-pre-btn')];
 
 ourTeamContainers.forEach((item, i) => {
     let containerDimensions = item.getBoundingClientRect();
